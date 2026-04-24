b6c249beb2c6bb772ba93b2c2bd52b2969c02d3b
CRFjonathas <jonaths.github@gmail.com>
2026-04-24 00:57:56 -0300

    refactor: cria classe Baralho que adciona cartas cadastradas em uma lista e as exibem

--- __main__.py
+++ __main__.py
@@ -1,12 +1,17 @@
 from classes.Cadastro import Cadastrar
-    
+from classes.Baralho import Baralho    
 
 def main():
-    carta1 = Cadastrar.cadastrar_carta()
-    carta2 = Cadastrar.cadastrar_carta()
+    baralho = Baralho()
 
-    print(carta1)
-    print(carta2)
+    carta1 = Cadastrar.cadastrar_carta() 
+    baralho.adicionar_carta(carta1)
+
+    carta2 = Cadastrar.cadastrar_carta()
+    baralho.adicionar_carta(carta2)
+
+    for cartas in baralho:
+        print(cartas) 
 
 if __name__ == "__main__":
     main()
